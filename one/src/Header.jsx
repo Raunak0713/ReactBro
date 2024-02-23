1cd5ed16d1b552571006153b7de293d5d0f3f2f9
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-02-23 08:02:21 +0530

    one

---
+++ one/src/Header.jsx
@@ -0,0 +1,13 @@
+export function Header(){
+    return(
+        <>
+            <h1>Welcome to my website</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Services</a></li>
+            </ul>
+        </>
+    );
+}
