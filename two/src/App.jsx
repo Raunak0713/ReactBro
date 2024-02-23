c16507831fe262084b33ffc06f75df4e04cc1a42
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-02-23 10:12:02 +0530

    Three

--- two/src/App.jsx
+++ two/src/App.jsx
@@ -2,7 +2,10 @@ import { Card } from "./Card";
 
 export function App() {
   return(
-    <Card />
+    <>
+      <Card />
+      <Card />
+    </>
   );
 }
 
--- two/src/Card.jsx
+++ two/src/Card.jsx
@@ -3,10 +3,13 @@ import profile from './assets/profile.jpeg'
 export function Card(){
     return(
         <div className="card">
-            <img src={profile}
-            alt="profile pic"></img>
-            <h2>Raunak</h2>
-            <p>Btech 3rd Year</p>
+            <img 
+                src={profile}
+                alt="profile pic" 
+                className='cardImage'
+            />
+            <h2 className='cardTitle'>Raunak</h2>
+            <p className='cardText'>Btech 3rd Year</p>
         </div>
     );
 }
