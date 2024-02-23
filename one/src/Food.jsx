4a293b7fed0ec8e9642438ddb320980ab0c3f7dc
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-02-23 09:07:06 +0530

    o

--- one/src/Food.jsx
+++ one/src/Food.jsx
@@ -1,1 +1,11 @@
-export function Food()
+export function Food(){
+    const food1 = "apple";
+    const food2 = "banana";
+    return(
+        <ul>
+            <li>Orange</li>
+            <li>{food1}</li> {/*Use the value of food1 variable */}
+            <li>{food2.toUpperCase()}</li> {/*Use the function to capitalize food2 variable*/}
+        </ul>
+    );
+}
